b9716c7e4c32863c90bb6d63d92538fe79b76efd
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-01-11 01:12:33 -0300

    n4 started

--- ALP/p1n4.js
+++ ALP/p1n4.js
@@ -5,3 +5,7 @@ var medidas =
         { label: "quarto2"  , largura: 2.7, comprimento:2 }, 
         { label: "banheiro" , largura: 1.5, comprimento:2 }
     ]; 
+
+var maiorC = medidas.map(l => {return {label: l.label, m2: l.largura*l.comprimento}});
+
+console.log(maiorC);
