c7a56465ca2faf6772878dcdc1faedb491492e7c
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-01-10 21:40:15 -0300

    Number 1

--- ALP/p1n1.js
+++ ALP/p1n1.js
@@ -1,3 +1,11 @@
-var notas = [8.0,7.0,8.3,9.0,4.5,6.0,3.7,5.0,7.1,9.5,6.0]; 
+let notas = [8.0, 7.0, 8.3, 9.0, 4.5, 6.0, 3.7, 5.0, 7.1, 9.5, 6.0]; 
 
-var 
+var media = notas.reduce((n, prox) => n+prox);
+var aprovado = notas.filter(n => n >= 6.0);
+var msg = notas.map((n) => {if (n >= 6.0) return 'Aprovado'; else return 'Reprovado' });
+
+media = media/11;
+
+console.log("a média foi: ", media);
+console.log("as notas aprovadas foram: ", aprovado);
+console.log(msg);
---
+++ ALP/p1n4.js
@@ -0,0 +1,7 @@
+var medidas = 
+    [
+        { label: "sala"     , largura: 4.5, comprimento:5 }, 
+        { label: "quarto1"  , largura: 3.5, comprimento:2 }, 
+        { label: "quarto2"  , largura: 2.7, comprimento:2 }, 
+        { label: "banheiro" , largura: 1.5, comprimento:2 }
+    ]; 
