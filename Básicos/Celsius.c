874dff130e56ae48bf26747b9c4c63c0dd0c9b17
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-05-01 13:42:21 -0300

    Update Celsius.c

--- Básicos/Celsius.c
+++ Básicos/Celsius.c
@@ -3,8 +3,8 @@
 int main (){
     float C, F;
 	printf("Digite uma temperatura em Graus Celsius: ");
-    scanf(" %f,", &C);
+    scanf("%f", &C);
     F = (9*C+160)/5;
-    printf("A temperatura em Fahrenheit é: %.4f", F); 
+    printf("A temperatura em Fahrenheit é: %.2f", F); 
     return 0;
 }
