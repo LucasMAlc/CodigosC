fb0c2b474f77f102ba0d4dcfc433be026473394f
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-05-08 18:35:43 -0300

    Update App.java

--- ALP/P2/Concorrente/P2N1/src/App.java
+++ ALP/P2/Concorrente/P2N1/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("O código da p2 não foi feito aqui :/");
     }
 }
