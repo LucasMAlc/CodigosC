f05c8b262368f00f89152020f7d720f2fb3f19d9
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2022-06-19 16:32:29 -0300

    Update Tabuada.c

--- C Codes/Tabuada.c
+++ C Codes/Tabuada.c
@@ -2,13 +2,18 @@
 
 int main(){
 
-    int n;
-    printf("Entre com um número");
-    scanf("%f", &n);
+    int n, vetor[10];
+    printf("Entre com um numero: ");
+    scanf("%d", &n);
+    
+    for (int i = 1; i < 10; i++){
+        vetor[i] = n * i;
+    }
 
-    for (int i = 0; i < 10; i++){
-
-
+    printf("Tabuada de %f: \n", n);
+    for (int j = 1; j < 10; j++){
+        printf("%f ", vetor[j]);
     }
     
+    return 0;
 }
